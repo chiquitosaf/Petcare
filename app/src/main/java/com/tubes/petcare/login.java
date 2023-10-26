42eb34abb34806b3dff829e638879bb99c60d546
chiquitosaf <chiquitosaf@gmail.com>
2023-10-26 17:17:11 +0700

    tambah halaman login

---
+++ app/src/main/java/com/tubes/petcare/login.java
@@ -0,0 +1,13 @@
+package com.tubes.petcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
