6fc66cf55a640b9d8e4245f60b068957e2f4861c
chiquitosaf <chiquitosaf@gmail.com>
2023-10-27 16:45:30 +0700

    update landing2 layout dan tambah fungsi tombol registrasi dan login

--- app/src/main/java/com/tubes/petcare/landing3.java
+++ app/src/main/java/com/tubes/petcare/landing3.java
@@ -2,7 +2,9 @@ package com.tubes.petcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class landing3 extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class landing3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing3);
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(v -> openLogin());
+
+        Button btn_register = (Button) findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(v -> openRegister());
+    }
+    public void openLogin(){
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+    }
+
+    public void openRegister(){
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
     }
 }
