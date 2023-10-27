5949f8c71c5b5d6f041f1efd01102326c9aac557
chiquitosaf <chiquitosaf@gmail.com>
2023-10-27 16:45:05 +0700

    update landing2 layout dan tambah fungsi tombol next

--- app/src/main/java/com/tubes/petcare/landing2.java
+++ app/src/main/java/com/tubes/petcare/landing2.java
@@ -2,7 +2,9 @@ package com.tubes.petcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class landing2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class landing2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing2);
+
+        Button btn_next = (Button) findViewById(R.id.btn_next);
+        btn_next.setOnClickListener(v -> openLanding3());
+    }
+
+    public void openLanding3(){
+        Intent intent = new Intent(this, landing3.class);
+        startActivity(intent);
     }
 }
